e0a9f30b875e61001d9bb00d30b367cfb5872fef
moustafa moungari <m.moungari@groupeonepoint.com>
2020-09-01 19:14:47 +0200

    modification de la classe main

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-
+        public static  int number = 10;
         System.out.println("Test git");
 
         }
